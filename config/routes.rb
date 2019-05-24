5b6b21f628fd9cac6a1587b0ed929cf2abfc06f8
kodera123 <b.b.015.k.k@gmail.com>
2019-05-24 18:08:55 +0900

    addroutes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  get 'statics/top'
+  get 'statics/second'
+  get 'statics/third'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
